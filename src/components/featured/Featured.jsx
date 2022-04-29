47be3d695bf75f15a443e4e412b062e28717bc5f
Ana Muraya <anamuraya9@gmail.com>
2022-04-29 13:45:35 +0300

    Updated the dashboard

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,19 @@
+import './featured.scss'
+
+const Featured = () => {
+  return (
+    <div className='featured'>
+      <div className="top">
+        <h1 className="title">Total Revenue</h1>
+
+      </div>
+      <div className="bottom">
+        <div className="featuredBar">
+          
+        </div>
+      </div>
+    </div>
+  )
+
+}
+export default  Featured
